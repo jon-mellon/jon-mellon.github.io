f73aab98c941153841bdeee596ea01bafcfb60d0
Jon Mellon <jonathan.mellon@nuffield.ox.ac.uk>
2023-07-14 11:11:51 -0400

    very minor fix

--- docs/dagpository.js
+++ docs/dagpository.js
@@ -787,7 +787,7 @@ createListHierarchy = function() {
   var i;
    for (i = 0; i < toggler.length; i++) {
     toggler[i].addEventListener("click", function() {
-      this.parentElement.querySelector("nested").classList.toggle(".active");
+      this.parentElement.querySelector(".nested").classList.toggle("active");
       this.classList.toggle("caret-down");
       
       
